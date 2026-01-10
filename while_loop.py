9ba0e3da3afc7df25eed83e26163b7010f5a3ae5
Neeraj <neeraj.cseh@gmail.com>
2026-01-10 22:27:58 +0530

    add

--- while_loop.py
+++ while_loop.py
@@ -1,4 +1,12 @@
-n = 5
-while (n > 0):
-    print (n)
-    n = n-1
+# n = 5
+# while (n > 0):
+#     print (n)
+#     n = n-1
+
+x = 5
+while (x>0):
+    print (x)
+    x = x - 1
+
+else:
+    print('counter is 0')
