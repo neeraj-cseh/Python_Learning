f91e20e9ca6e80b7dfeb9aeca2a0376edeb913cb
Neeraj <neeraj.cseh@gmail.com>
2026-01-10 23:35:34 +0530

    add

---
+++ break.py
@@ -0,0 +1,12 @@
+# for i in range (12):
+#     if (i == 10):
+#         break
+#     print("5X", i+1, "=", 5*(i+1))
+
+# print ("5th table is completed")
+
+for i in range (12):
+    if (i == 10):
+        print("Skip this iteration")
+        continue
+    print ("5X", i, "=", 5*i)
