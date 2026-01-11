1d9544e6cb515ead683d9e4a9bd022a36f7b6054
Neeraj <neeraj.cseh@gmail.com>
2026-01-11 18:58:33 +0530

    add

--- lists.py
+++ lists.py
@@ -6,7 +6,70 @@
 # else:
 #     print ("No")
 
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print (x[0:9])
-print (x[0:9:2])
-print (x[0:9:3])
+# x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print (x[0:9])
+# print (x[0:9:2])
+# print (x[0:9:3])
+
+# x = [a for a in range(5)]
+# print (x)
+
+# x = [a+a for a in range(5)]
+# print (x)
+
+# x = [a for a in range(10) if a%2 == 0]
+# print (x)
+
+# names = ["Neeraj", "Srinivas", "Mala", "Sir"]
+# names_i = [item for item in names if "i" in item]
+# print (names_i)
+
+# names = ["Neeraj", "Srinivas", "Mala", "Sir"]
+# names_4 = [item for item in names if len(item) > 4]
+# print (names_4)
+
+# x = [10, 1, 4, 3, 2, 8, 5, 9, 6]
+# x.sort()
+# print (x)
+
+# x = [10, 1, 4, 3, 2, 8, 5, 9, 6]
+# x.sort(reverse=True)
+# print (x)
+
+# x = [10, 9, 8, 4, 7]
+# x.reverse()
+# print (x)
+
+# x = [4, 2, 5, 4, 2, 5]
+# print (x.index(2))
+
+# x = [4, 2, 5, 4, 2, 5]
+# print (x.count(2))
+
+# x = [5, 6, 7, 8, 9]
+# y = x
+# y[0] = 0
+# print (x)
+# print (y)
+
+# x = [1, 2, 3]
+# x.append(4)
+# print (x)
+
+# x = [1, 3, 4]
+# x.insert(1, 2)
+# print (x)
+
+# x = [0, 2, 4, 6]
+# y = [1, 3, 5, 7]
+# x.extend(y)
+# print(x)
+
+# x = [1, 2, 3, 4]
+# y = [5, 6, 7, 8]
+# print (x+y)
+
+# x = [1, 2, 3, 4]
+# y = [5, 6, 7, 8]
+# z = (x+y)
+# print (z)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+names = ["Neeraj", "Srinivas", "Mala", "Sir"]
+names_i = [item for item in names if "i" in item]
+print (names_i)
