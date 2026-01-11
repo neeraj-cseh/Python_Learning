efd301347da1876a4fcdb16c6904d8fa57c8032d
Neeraj <neeraj.cseh@gmail.com>
2026-01-11 19:33:19 +0530

    add

--- tuples.py
+++ tuples.py
@@ -1,4 +1,24 @@
 # x = (1, 2, 3, 4)
 # print (type(x), x)
 
+# countries = ("Spain", "Italy", "India", "England", "Germany")
+# temp = list(countries)
+# temp.append("Russia")
+# temp.pop(3)
+# temp[2] = "Finland"
+# countries = tuple(temp)
+# print (type(countries))
+# print (countries)
 
+# a = (1, 2, 3)
+# b = (4, 5, 6)
+# c = a + b
+# print (type(c))
+# print (c)
+
+# x = (0, 2, 1, 1, 2, 5, 6, 8, 1, 2)
+# # print (x.index(1))
+# print (x.index(1, 4, 9))
+
+# x = (1, 2, 3, 4, 5, 6)
+# print (len(x))
