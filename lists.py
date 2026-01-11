dde7845899fd785e488fb455d31e80a4dd5d45c3
Neeraj <neeraj.cseh@gmail.com>
2026-01-11 17:13:25 +0530

    add

--- lists.py
+++ lists.py
@@ -1,7 +1,12 @@
-x = [1, 4, "Neeraj", 8]
+# x = [1, 4, "Neeraj", 8]
 
-if 1 in x:
-    print ("Yes")
+# if 1 in x:
+#     print ("Yes")
 
-else:
-    print ("No")
+# else:
+#     print ("No")
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print (x[0:9])
+print (x[0:9:2])
+print (x[0:9:3])
