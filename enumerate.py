415ea8a0493738260389c50a349b42e54585199d
Neeraj <neeraj.cseh@gmail.com>
2026-01-12 12:21:01 +0530

    add

---
+++ enumerate.py
@@ -0,0 +1,19 @@
+# marks = [10, 11, 12, 15, 10]
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if index == 3:
+#         print("Great Neeraj")
+#     index += 1
+
+# marks = [10, 11, 12, 15, 10]
+# for index, mark in enumerate(marks):
+#     print(mark)
+#     if index == 3:
+#         print("Great Neeraj")
+
+# marks = [10, 11, 12, 15, 10]
+# for index, mark in enumerate(marks, start=1):
+#     print(mark)
+#     if index == 3:
+#         print("Great Neeraj")
--- try_except.py
+++ try_except.py
@@ -40,4 +40,3 @@
 #         print("Executed")
 # z = fun1()
 # print(z)
-
