30ff1f9496b52c993196efef57b612343ab253ab
Neeraj <neeraj.cseh@gmail.com>
2026-01-13 05:06:25 +0530

    add

---
+++ os.py
@@ -0,0 +1,13 @@
+# import os
+# cmd = 'date'
+# os.system(cmd)
+
+# directiory = 'data'
+
+# import os
+# if (not os.path.exists(directiory)):
+#     os.mkdir(directiory)
+
+# for i in range (0, 100):
+#     files = f"{directiory}/Task {i+1}"
+#     os.mkdir(files)
