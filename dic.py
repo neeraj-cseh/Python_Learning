58a7b87e65774247b3e230e6e607f991d2d24265
Neeraj <neeraj.cseh@gmail.com>
2026-01-12 08:43:22 +0530

    add

---
+++ dic.py
@@ -0,0 +1,31 @@
+# info = {'name':'Neeraj', 'age':23, 'eligible':True}
+# # print(info)
+# # print(info['age'])
+# # print(info.get('name'))
+# # print(info.values())
+# # print(info.keys())
+
+# # for key in info.keys():
+# #     print(info[key])
+
+# print(info.items())
+
+info = {'name':'Neeraj', 'age':24}
+# info.update({'age':23})
+# info.update({'sex':'M'})
+# print(info)
+
+# info.clear()
+# print(info)
+
+# info.pop('age')
+# print(info)
+
+# info.popitem()
+# print(info)
+
+# del info
+# print(info)
+
+# del info['name']
+# print(info)
