1745413cc56ed11d1c4472ef3a59aa1c8731aa76
Neeraj <neeraj.cseh@gmail.com>
2026-01-12 11:28:14 +0530

    add

---
+++ else_loop.py
@@ -0,0 +1,27 @@
+# for i in range(6):
+#     print (i)
+# else:
+#     print("Done")
+
+# for i in range(6):
+#     print(i)
+#     if (i == 4):
+#         break
+# else:
+#     print("Done")
+
+# i = 0
+# while i<7:
+#     print(i)
+#     i = i + 1
+# else:
+#     print("Done")
+
+# i = 0
+# while i<7:
+#     print(i)
+#     i = i + 1
+#     if (i == 4):
+#         break
+# else:
+#     print("Done")
---
+++ try_except.py
@@ -0,0 +1,43 @@
+# a = input("Enter the number: ")
+# print (f"Multiplication table of {a} is: ")
+# for i in range (1, 11):
+#     print(f"{int(a)} X {i} = {int(a)*i}")
+
+# print("Some imp code")
+# print("End of program")
+
+# a = input("Enter the number: ")
+# print (f"Multiplication table of {a} is: ")
+
+# try:
+#     for i in range (1, 11):
+#         print(f"{int(a)} X {i} = {int(a)*i}")
+# except:
+#     print("Invalid input")
+
+# print("Some imp code")
+# print("End of program")
+
+# try:
+#     num = int(input("Enter an integer: "))
+#     a = [6, 3]
+#     print (a[num])
+# except ValueError:
+#     print("Not an integer")
+# except IndexError:
+#     print("Invalid index")
+
+# def fun1():
+#     try:
+#         x = [1, 5, 6, 7]
+#         y = int(input("Enter the index: "))
+#         print(x[y])
+#         return 1
+#     except:
+#         print("Error")
+#         return 0
+#     finally:
+#         print("Executed")
+# z = fun1()
+# print(z)
+
