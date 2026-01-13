7e0a4f03b8c62a76c2592732808099f46e8a5583
Neeraj <neeraj.cseh@gmail.com>
2026-01-13 10:46:28 +0530

    add

---
+++ file.py
@@ -0,0 +1,20 @@
+# f = open('test.txt', 'r')
+# text = f.read()
+# print(text)
+# f.close
+
+# f = open('text.txt', 'w')
+# i = 0
+# for i in range (0, 6):
+#     f.write(f"Hello {i}")
+# f.close()
+
+# f = open('text.txt', 'a')
+# i = 0
+# for i in range (0, 6):
+#     f.write(f"Hello {i}")
+# f.close()
+
+# with open('text.txt', 'r') as f:
+#     text = f.read()
+#     print (text)
