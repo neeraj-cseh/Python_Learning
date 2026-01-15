901cc353664bd927730ad76185f64368e520b196
Neeraj <neeraj.cseh@gmail.com>
2026-01-15 10:55:28 +0530

    add

--- file.py
+++ file.py
@@ -18,3 +18,53 @@
 # with open('text.txt', 'r') as f:
 #     text = f.read()
 #     print (text)
+
+# f = open('text.txt', 'r')
+# while True:
+#     line = f.readline()
+#     if not line:
+#         print("Completed")
+#         break
+#     print(line)
+
+# f = open('text.txt', 'w')
+# lines = ['A\n', 'B\n', 'C\n']
+# f.writelines(lines)
+# f.close()
+
+# f = open('text.txt', 'a')
+# lines = ['A\n', 'B\n', 'C\n']
+# f.writelines(lines)
+# f.close()
+
+# f = open('text.txt', 'w')
+# lines = ['A', 'B', 'C']
+# for line in lines:
+#     f.write('\n' + line + '\n')
+# f.close()
+
+
+# f = open('text1.txt', 'w')
+# lines = ['1234567890Neeraj']
+# for line in lines:
+#     f.write(line)
+# f.close()
+
+# with open('text1.txt', 'r') as f:
+#     f.seek(10)
+#     data = f.read(6)
+#     print(data)
+
+# with open('text1.txt', 'r') as f:
+#     data = f.read(10)
+#     current_position = f.tell()
+#     f.seek(current_position)
+#     print(current_position)
+#     print(data)
+
+# with open('text1.txt', 'w') as f:
+#     f.write("Hello World")
+#     f.truncate(5)
+
+# with open('text1.txt', 'r') as f:
+#     print(f.read())
---
+++ map.py
@@ -0,0 +1,22 @@
+#Map() function
+# def sum(x):
+#     return x + x
+# print(sum(5))
+# l = [1, 2, 3, 4, 5]
+# newl = list(map(sum, l))
+# print(newl)
+
+#Filter() function
+# def filter_function(x):
+#     return x>3
+# l = [1, 2, 3, 4, 5, 6]
+# newl = list(filter(filter_function, l))
+# print(newl)
+
+#Reduce() function
+# from functools import reduce
+# def sum(x, y):
+#     return x + y
+# l = [1, 2, 3, 4, 5]
+# ans = reduce(sum, l)
+# print(ans)
