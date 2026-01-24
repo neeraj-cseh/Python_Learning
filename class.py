936a8985a98da541a14c6688acc5bde572d0a76c
Neeraj <neeraj.cseh@gmail.com>
2026-01-24 14:13:40 +0530

    add

---
+++ class.py
@@ -0,0 +1,26 @@
+# class Details:
+#     name = "Neeraj"
+#     age = 23
+#     def info(self):
+#         print("My name is", self.name, "and I'm", self.age, "years old.")
+
+# a = Details()
+# a.info()
+# b = Details()
+# b.name = "Srinivas"
+# b.age = 22
+# b.info()
+
+class Personal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print("My name is", self.name, "and I am", self.age, "years old.")
+
+    # def info(self):
+    #     print("My name is", self.name, "and I am", self.age, "years old.")
+
+a = Personal("Neeraj", 23)
+# a.info()
+b = Personal("Srinivas", 22)
+# b.info()
