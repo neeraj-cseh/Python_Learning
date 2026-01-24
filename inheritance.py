e881d1060090848453c13dbc8a0a17ae3d49e784
Neeraj <neeraj.cseh@gmail.com>
2026-01-25 01:34:01 +0530

    add

--- inheritance.py
+++ inheritance.py
@@ -1,16 +1,16 @@
-class Parent:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Parent:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def Details(self):
-        print(f"{self.name} age is {self.age}")
+#     def Details(self):
+#         print(f"{self.name} age is {self.age}")
 
-class Child(Parent):
-    def Language(self):
-        print("Mother tongue is Telugu")
+# class Child(Parent):
+#     def Language(self):
+#         print("Mother tongue is Telugu")
 
-a = Parent("Neeraj", 23)
-a.Details()
-b = Child("Srinivas", 22)
-b.Language()
+# a = Parent("Neeraj", 23)
+# a.Details()
+# b = Child("Srinivas", 22)
+# b.Language()
---
+++ modifier.py
@@ -0,0 +1,21 @@
+# class Public:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# a = Public("Neeraj", 23)
+# print(a.name, a.age)
+
+# class Private:
+#     def __init__(self):
+#         self.__name = "Neeraj"
+
+# a = Private()
+# print(a._Private__name)
+
+class Protected:
+    def __init__(self):
+        self._name = "Neeraj"
+
+a = Protected()
+print(a._name)
