2528e86a5b181efde576dcb4873e37268318dbaf
Neeraj <neeraj.cseh@gmail.com>
2026-01-24 16:54:33 +0530

    add

---
+++ decorator.py
@@ -0,0 +1,17 @@
+# def greet(fx):
+#     def mfx(*args, **kwargs):
+#         print("Good Morning")
+#         fx(*args, **kwargs)
+#         print("Thank you for using the function")
+#     return mfx
+
+# @greet
+# def hello():
+#     print("Hello World...!")
+
+# @greet
+# def add(a, b):
+#     print(a + b)
+
+# hello()
+# add(1, 2)
