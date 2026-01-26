9badf01446d54bed7f5d55b3310010a4fba0543c
Neeraj <neeraj.cseh@gmail.com>
2026-01-26 20:15:25 +0530

    add

---
+++ civ.py
@@ -0,0 +1,19 @@
+# class Books:
+#     library_name = "Library"
+#     noofbooks = 0
+#     def __init__(self, name):
+#         self.name = name
+#         Books.noofbooks += 1
+#     def showbooks(self):
+#         print (f"The name of the book is {self.name}, and there are total {self.noofbooks} books in this {self.library_name}.")
+
+# a = Books("Neeraj")
+# b = Books("Srinivas")
+# c = Books("Mala")
+# d = Books("Python")
+# e = Books("C++")
+# Books.showbooks(a)
+# Books.showbooks(b)
+# Books.showbooks(c)
+# Books.showbooks(d)
+# Books.showbooks(e)
