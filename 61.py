6b7ba804af7940f2daebdaa02905adac8e7a8ca5
Neeraj <neeraj.cseh@gmail.com>
2026-05-24 04:18:54 +0530

    add

--- 61.py
+++ 61.py
@@ -1,22 +1,22 @@
-class Parent:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Parent:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
     
-    def info(self):
-        print(f"{self.name} is {self.age} years old.")
+#     def info(self):
+#         print(f"{self.name} is {self.age} years old.")
     
-class Child(Parent):
-    def __init__(self, name, age, lang):
-        super().__init__(name, age)
-        self.lang = lang
+# class Child(Parent):
+#     def __init__(self, name, age, lang):
+#         super().__init__(name, age)
+#         self.lang = lang
 
-    def speak(self):
-        print(f"{self.name} can speak {self.lang}.")
+#     def speak(self):
+#         print(f"{self.name} can speak {self.lang}.")
 
-p1 = Parent("Neeraj", 25)
-p1.info()
+# p1 = Parent("Neeraj", 25)
+# p1.info()
 
-c1 = Child("Srinivas", 15, "Telugu")
-c1.info()
-c1.speak()
+# c1 = Child("Srinivas", 15, "Telugu")
+# c1.info()
+# c1.speak()
---
+++ 69.py
@@ -0,0 +1,20 @@
+# class Employee:
+#     company = "Apple"
+#     def show(self):
+#         print(f"{self.name} is from {self.company}.")
+
+#     @classmethod
+#     def chcom(cls, newcom):
+#         cls.company = newcom
+# e1 = Employee()
+# e1.name = "Neeraj"
+# e1.show()
+
+# print(Employee.company)
+
+# Employee.chcom("Google")
+# e2 = Employee()
+# e2.name = "Srinivas"
+# e2.show()
+
+# print(Employee.company)
