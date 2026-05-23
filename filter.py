1b0eb1060f627e03bebddd7fe2458559e2be4d84
Neeraj <neeraj.cseh@gmail.com>
2026-05-23 20:20:51 +0530

    add

--- filter.py
+++ filter.py
@@ -1,2 +1,15 @@
-name = input()
-print(name, "hello!")
+# def map_f(x):
+#     return x + x
+# arr = [1, 2, 3, 4]
+# print(list(map(map_f, arr)))
+
+# def filter_f(x):
+#     return x > 5
+# arr = [1, 2, 3, 4, 5, 6, 7, 8]
+# print(list(filter(filter_f, arr)))
+
+# from functools import reduce
+# numbers = [1, 2, 3, 4]
+# def reduce_f(x, y):
+#     return x + y
+# print(reduce(reduce_f, numbers))
