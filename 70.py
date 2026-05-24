b07687ef4cc317903f47278d472aa8bfc67d7229
Neeraj <neeraj.cseh@gmail.com>
2026-05-25 00:09:37 +0530

    add

---
+++ 70.py
@@ -0,0 +1,12 @@
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         print(f"Person created: {self.name}, {self.age}")
+
+#     @classmethod
+#     def fromstr(cls, string):
+#         return cls(string.split("-")[0], string.split("-")[1])
+
+# p1 = Person("Neeraj", 23)
+# p2 = Person.fromstr("Srinivas-25")
