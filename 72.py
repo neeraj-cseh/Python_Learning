fff36852050909b2f1ce4d046892d5f33272423e
Neeraj <neeraj.cseh@gmail.com>
2026-05-25 00:48:00 +0530

    add

---
+++ 72.py
@@ -0,0 +1,11 @@
+class Parent:
+    def parent_method(self):
+        print("This is the parent method.")
+    
+class Child(Parent):
+    def child_method(self):
+        print("This is the child method.")
+        super().parent_method()
+
+a = Child()
+a.child_method()
