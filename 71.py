cd2f84981fd1b33c76f1c0f9777dc1cd0276c747
Neeraj <neeraj.cseh@gmail.com>
2026-05-25 00:40:29 +0530

    add

---
+++ 71.py
@@ -0,0 +1,24 @@
+# dir() Method
+# x = [1, 2, 3]
+# a = dir(x)
+# print(a)
+
+# __dict__ Attribute
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         print(f"{self.name} is of {self.age} years old.")
+
+# p1 = Person("Neeraj", 23)
+# print(p1.__dict__)
+
+# help() Method
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         print(f"{self.name} is of {self.age} years old.")
+
+# p1 = Person("Neeraj", 23)
+# help(Person)
